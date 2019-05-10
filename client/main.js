26b187a2f7274db236ee9dd62dcb0caab5ecd2b3
paulinavita <paulinadavita@gmail.com>
2019-05-10 10:33:32 +0700

    delete directives, move fx to mounted

--- client/main.js
+++ client/main.js
@@ -6,9 +6,8 @@ new Vue({
     image : '',
     url : "http://iskconofescondido.com/wp-content/uploads/2019/03/blangko-kosong-e-ktp.jpg",
     ktp : {
-        nama : 'Azhar',
-        nik : '1234',
-        ttl : '1234',
+        nama : '',
+        nik : '',
         ttl : '',
         jk : '',
         goldar : '',
@@ -20,17 +19,26 @@ new Vue({
         kawin : '',
         pekerjaan : '',
         kwn : '',
-        berlaku : 'SEUMUR HIDUP'
+        berlaku : ''
 
     },
     cwidth: "" , 
-    cheight: "" ,
-    yey : null
+    cheight: "" 
 
    },
    created() {
     this.cwidth = $('img').width()
     this.cheight = $('img').height();
+   },
+   mounted() {
+    var canvasElement = document.getElementById('canvas');
+    var ctx = canvasElement.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    var imageObj = new Image();
+    imageObj.src = './ktp.png';
+    imageObj.onload = function () {
+        ctx.drawImage(imageObj, 0, 0);
+    }
    },
     methods: {
         updateXY: function(event) {
@@ -38,54 +46,104 @@ new Vue({
             this.y = event.offsetY;
         },
         screenshot() {
-            console.log(this.yey);
-            
-            // console.log(document.getElementById('can'))
-            // html2canvas(document.getElementById('canvas'), {allowTaint: true, allowCORS : true })
-            // .then( (canvas) => {
+
+            html2canvas(document.getElementById('canvas'), {allowTaint: true })
+            .then( (canvas) => {
                 
-            //     console.log(canvas, 'apa');   
-            //     document.body.appendChild(canvas);
-            //     console.log(canvas.toDataURL('image/png'), '------')
-            // })
+                console.log(canvas, 'apa');   
+                document.body.appendChild(canvas);
+                console.log(canvas.toDataURL('image/png'), '------')
+            })
         },
         getImage(event) {
             this.image = event.target.files[0]
             console.log('disini', this.image, '//////////');
-        }
+        },
     },
-    directives : {
-        insertData : function (canvasElement, ktp) {
-            console.log(ktp.value);
-            console.log(canvasElement, 'ini apa');
-            
-            this.yey = canvasElement
+    watch: {
+        'ktp.nama': function() {
+            console.log('ktp changed');
+            var canvasElement = document.getElementById('canvas');
             var ctx = canvasElement.getContext("2d");
             ctx.clearRect(0, 0, canvas.width, canvas.height)
-            ctx.drawImage($('img').get(0), 0, 0);
-            ctx.fillText(ktp.value.nik, 115, 75);
-            ctx.fillText(ktp.value.nama, 125, 100);
-            ctx.fillText(ktp.value.ttl, 125, 115);
-            ctx.fillText(ktp.value.jk, 125, 130);
-            ctx.fillText(ktp.value.goldar, 325, 129);
-            ctx.fillText(ktp.value.alamat, 125, 145);
-            ctx.fillText(ktp.value.rt, 125, 160);
-            ctx.fillText(ktp.value.kel, 125, 175);
-            ctx.fillText(ktp.value.kec, 125, 190);
-            ctx.fillText(ktp.value.agama, 125, 205);
-            ctx.fillText(ktp.value.kawin, 125, 220);
-            ctx.fillText(ktp.value.pekerjaan, 125, 235);
-            ctx.fillText(ktp.value.kwn, 125, 250);
-            ctx.fillText(ktp.value.berlaku, 125, 265);
-
-            // var imageObj = new Image();
-            // imageObj.src = 'https://1.bp.blogspot.com/-sporyQjyNms/WPBDKcnMUhI/AAAAAAAAGnE/HSIOH0oN9788ze1z4LKES_lZj0dUwbgGQCLcB/s1600/Pas%2BFoto%2BSBMPTN%2BTerbaru.png';
-            // imageObj.onload = function () {
-            //     ctx.drawImage(imageObj, 363, 60, 116, 150);
-            // }
-            // ctx.save()
-            
+            var imageObj = new Image();
+            imageObj.src = './ktp.png';
+            imageObj.onload = () => {
+                ctx.drawImage(imageObj, 0, 0);
+                ctx.fillText(this.ktp.nik, 115, 75);
+                ctx.fillText(this.ktp.nama, 125, 100);
+                ctx.fillText(this.ktp.ttl, 125, 115);
+                ctx.fillText(this.ktp.jk, 125, 130);
+                ctx.fillText(this.ktp.goldar, 325, 129);
+                ctx.fillText(this.ktp.alamat, 125, 145);
+                ctx.fillText(this.ktp.rt, 125, 160);
+                ctx.fillText(this.ktp.kel, 125, 175);
+                ctx.fillText(this.ktp.kec, 125, 190);
+                ctx.fillText(this.ktp.agama, 125, 205);
+                ctx.fillText(this.ktp.kawin, 125, 220);
+                ctx.fillText(this.ktp.pekerjaan, 125, 235);
+                ctx.fillText(this.ktp.kwn, 125, 250);
+                ctx.fillText(this.ktp.berlaku, 125, 265);
+            }
+        },
+        'ktp.nik': function() {
+            var canvasElement = document.getElementById('canvas');
+            var ctx = canvasElement.getContext("2d");
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            var imageObj = new Image();
+            imageObj.src = './ktp.png';
+            imageObj.onload = () => {
+                ctx.drawImage(imageObj, 0, 0);
+                ctx.fillText(this.ktp.nik, 115, 75);
+                ctx.fillText(this.ktp.nama, 125, 100);
+                ctx.fillText(this.ktp.ttl, 125, 115);
+                ctx.fillText(this.ktp.jk, 125, 130);
+                ctx.fillText(this.ktp.goldar, 325, 129);
+                ctx.fillText(this.ktp.alamat, 125, 145);
+                ctx.fillText(this.ktp.rt, 125, 160);
+                ctx.fillText(this.ktp.kel, 125, 175);
+                ctx.fillText(this.ktp.kec, 125, 190);
+                ctx.fillText(this.ktp.agama, 125, 205);
+                ctx.fillText(this.ktp.kawin, 125, 220);
+                ctx.fillText(this.ktp.pekerjaan, 125, 235);
+                ctx.fillText(this.ktp.kwn, 125, 250);
+                ctx.fillText(this.ktp.berlaku, 125, 265);
+            }
         }
+    },
+    // directives : {
+    //     insertData : function (canvasElement, ktp, posX, posY) {
+    //         console.log('insertData invoked');
+    //         // console.log(ktp.value);
+            
+    //         var ctx = canvasElement.getContext("2d");
+    //         ctx.clearRect(0, 0, canvas.width, canvas.height)
+    //         ctx.drawImage($('img').get(0), 0, 0);
+    //         ctx.fillText(ktp.value.nik, 115, 75);
+    //         ctx.fillText(ktp.value.nama, 125, 100);
+    //         ctx.fillText(ktp.value.ttl, 125, 115);
+    //         ctx.fillText(ktp.value.jk, 125, 130);
+    //         ctx.fillText(ktp.value.goldar, 325, 129);
+    //         ctx.fillText(ktp.value.alamat, 125, 145);
+    //         ctx.fillText(ktp.value.rt, 125, 160);
+    //         ctx.fillText(ktp.value.kel, 125, 175);
+    //         ctx.fillText(ktp.value.kec, 125, 190);
+    //         ctx.fillText(ktp.value.agama, 125, 205);
+    //         ctx.fillText(ktp.value.kawin, 125, 220);
+    //         ctx.fillText(ktp.value.pekerjaan, 125, 235);
+    //         ctx.fillText(ktp.value.kwn, 125, 250);
+    //         ctx.fillText(ktp.value.berlaku, 125, 265);
+
+    //         // var imageObj = new Image();
+    //         // imageObj.src = 'https://1.bp.blogspot.com/-sporyQjyNms/WPBDKcnMUhI/AAAAAAAAGnE/HSIOH0oN9788ze1z4LKES_lZj0dUwbgGQCLcB/s1600/Pas%2BFoto%2BSBMPTN%2BTerbaru.png';
+    //         // imageObj.onload = function () {
+    //         //     ctx.drawImage(imageObj, 363, 60, 116, 150);
+    //         // }
+    //         // ctx.save()
+
+    //         console.log(canvasElement);
+            
+    //     }
         
-    }
+    // }
 })
